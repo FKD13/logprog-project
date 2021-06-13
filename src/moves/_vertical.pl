d16cb291993f5172a2618469588ffc081bd6e471
FKD13 <francisklinck@gmail.com>
2021-06-13 12:45:23 +0200

    change elements in move list - vertical

--- src/moves/_vertical.pl
+++ src/moves/_vertical.pl
@@ -2,22 +2,22 @@
 
 :- use_module('../utils/board_utils').
 
-get_vertical_moves(Board, Color, R/C, AboveMoves-T) :-
+get_vertical_moves(Board, Piece, R/C, AboveMoves-T) :-
     RPlus is R + 1,
     RMin is R - 1,
-    get_above_moves(Board, Color, RMin /C, AboveMoves-LowerMoves),
-    get_lower_moves(Board, Color, RPlus/C, LowerMoves-T).
+    get_above_moves(Board, Piece, R/C, RMin /C, AboveMoves-LowerMoves),
+    get_lower_moves(Board, Piece, R/C, RPlus/C, LowerMoves-T).
 
-get_above_moves(_, _, 0/_, T-T) :- !.
-get_above_moves(Board, Color, R/C, T-T) :- get_piece_at(Board, R/C, p(Color, _)), !.
-get_above_moves(Board, _    , R/C, [ R/C | T ]-T) :- get_piece_at(Board, R/C, p(_, _)), !.
-get_above_moves(Board, Color, R/C, [ R/C | Moves ]-T) :-
+get_above_moves(_    , _          , _, 0/_, T-T) :- !.
+get_above_moves(Board, p(Color, _), _, R/C, T-T) :- get_piece_at(Board, R/C, p(Color, _)), !.
+get_above_moves(Board, Piece      , O, R/C, [ m(Piece, O, R/C) | T ]-T) :- get_piece_at(Board, R/C, p(_, _)), !.
+get_above_moves(Board, Piece      , O, R/C, [ m(Piece, O, R/C) | Moves ]-T) :-
     RMin is R - 1,
-    get_above_moves(Board, Color, RMin/C, Moves-T).
+    get_above_moves(Board, Piece, O, RMin/C, Moves-T).
 
-get_lower_moves(_, _, 9/_, X-X) :- !.
-get_lower_moves(Board, Color, R/C, T-T) :- get_piece_at(Board, R/C, p(Color, _)), !.
-get_lower_moves(Board, _    , R/C, [ R/C | T ]-T) :- get_piece_at(Board, R/C, p(_, _)), !.
-get_lower_moves(Board, Color, R/C, [ R/C | Moves ]-T) :-
+get_lower_moves(_    , _          , _, 9/_, X-X) :- !.
+get_lower_moves(Board, p(Color, _), _, R/C, T-T) :- get_piece_at(Board, R/C, p(Color, _)), !.
+get_lower_moves(Board, Piece      , O, R/C, [ m(Piece, O, R/C) | T ]-T) :- get_piece_at(Board, R/C, p(_, _)), !.
+get_lower_moves(Board, Piece      , O, R/C, [ m(Piece, O, R/C) | Moves ]-T) :-
     RPlus is R + 1,
-    get_lower_moves(Board, Color, RPlus/C, Moves-T).
+    get_lower_moves(Board, Piece, O, RPlus/C, Moves-T).
--- tests/moves/_vertical.pl
+++ tests/moves/_vertical.pl
@@ -2,25 +2,30 @@
 :- use_module('../../src/moves/_vertical').
 :- use_module('../test_utils').
 
-test(get_vertical_moves, [setup(start_board(SB)), true(X = [4/3,5/3,6/3,7/3])]) :-
-    get_vertical_moves(SB, w, 3/3, X-[]).
+test(get_vertical_moves, [setup(start_board(SB)), true(X = [m(p(w,_),Old,4/3),m(p(w,_),Old,5/3),m(p(w,_),Old,6/3),m(p(w,_),Old,7/3)])]) :-
+    Old = 3/3,
+    get_vertical_moves(SB, p(w, _), Old, X-[]).
 
-test(get_vertical_moves, [setup(start_board(SB)), true(X = [3/7,5/7,6/7,7/7])]) :-
-    get_vertical_moves(SB, w, 4/7, X-[]).
+test(get_vertical_moves, [setup(start_board(SB)), true(X = [m(p(w,_),Old,3/7),m(p(w,_),Old,5/7),m(p(w,_),Old,6/7),m(p(w,_),Old,7/7)])]) :-
+    Old = 4/7,
+    get_vertical_moves(SB, p(w, _), Old, X-[]).
 
-test(get_vertical_moves, [setup(start_board(SB)), true(X = [6/3,5/3,4/3,3/3,8/3])]) :-
-    get_vertical_moves(SB, w, 7/3, X-[]).
+test(get_vertical_moves, [setup(start_board(SB)), true(X = [m(p(w,_),Old,6/3),m(p(w,_),Old,5/3),m(p(w,_),Old,4/3),m(p(w,_),Old,3/3),m(p(w,_),Old,8/3)])]) :-
+    Old = 7/3,
+    get_vertical_moves(SB, p(w, _), Old, X-[]).
 
 test(get_vertical_moves_corners, [setup(start_board(SB)), true(X = [])]) :-
-    get_vertical_moves(SB, w, 1/1, X-[]).
+    get_vertical_moves(SB, p(w, _), 1/1, X-[]).
 
 test(get_vertical_moves_corners, [setup(start_board(SB)), true(X = [])]) :-
-    get_vertical_moves(SB, w, 1/8, X-[]).
+    get_vertical_moves(SB, p(w, _), 1/8, X-[]).
 
-test(get_vertical_moves_corners, [setup(start_board(SB)), true(X = [7/8])]) :-
-    get_vertical_moves(SB, w, 8/8, X-[]).
+test(get_vertical_moves_corners, [setup(start_board(SB)), true(X = [m(p(w,_),Old,7/8)])]) :-
+    Old = 8/8,
+    get_vertical_moves(SB, p(w, _), Old, X-[]).
 
-test(get_vertical_moves_corners, [setup(start_board(SB)), true(X = [7/1])]) :-
-    get_vertical_moves(SB, w, 8/1, X-[]).
+test(get_vertical_moves_corners, [setup(start_board(SB)), true(X = [m(p(w,_),Old,7/1)])]) :-
+    Old = 8/1,
+    get_vertical_moves(SB, p(w, _), Old, X-[]).
 
 :- end_tests('moves/_vertical').
