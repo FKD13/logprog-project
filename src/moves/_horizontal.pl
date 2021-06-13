de34b9fd60f26facf68b16d3f9059fed981dc599
FKD13 <francisklinck@gmail.com>
2021-06-13 12:36:41 +0200

    change elements in move list - horizontal

--- src/moves/_horizontal.pl
+++ src/moves/_horizontal.pl
@@ -1,22 +1,22 @@
 :- module('_horizontal', [get_horizontal_moves/4]).
 
-get_horizontal_moves(Board, Color, R/C, LeftMoves-T) :-
+get_horizontal_moves(Board, Piece, R/C, LeftMoves-T) :-
     arg(R, Board, Row),
     CMin is C - 1,
     CPlus is C + 1,
-    get_left_moves( Row, Color, R/CMin , LeftMoves-RightMoves),
-    get_right_moves(Row, Color, R/CPlus, RightMoves-T).
+    get_left_moves( Row, Piece, R/C, R/CMin , LeftMoves-RightMoves),
+    get_right_moves(Row, Piece, R/C, R/CPlus, RightMoves-T).
     
-get_left_moves(_, _, _/0, T-T) :- !.
-get_left_moves(Row, Color, _/C, T-T) :- arg(C, Row, p(Color, _)), !.
-get_left_moves(Row, _    , R/C, [ R/C | T ]-T) :- arg(C, Row, p(_, _)), !.
-get_left_moves(Row, Color, R/C, [ R/C | Moves ]-T) :-
+get_left_moves(_  , _          , _, _/0, T-T) :- !.
+get_left_moves(Row, p(Color, _), _, _/C, T-T) :- arg(C, Row, p(Color, _)), !.
+get_left_moves(Row, Piece      , O, R/C, [ m(Piece, O, R/C) | T ]-T) :- arg(C, Row, p(_, _)), !.
+get_left_moves(Row, Piece      , O, R/C, [ m(Piece, O, R/C) | Moves ]-T) :-
     CMin is C - 1,
-    get_left_moves(Row, Color, R/CMin, Moves-T).
+    get_left_moves(Row, Piece, O, R/CMin, Moves-T).
 
-get_right_moves(_, _, _/9, T-T) :- !.
-get_right_moves(Row, Color, _/C, T-T) :- arg(C, Row, p(Color, _)), !.
-get_right_moves(Row, _    , R/C, [ R/C | T ]-T) :- arg(C, Row, p(_, _)), !.
-get_right_moves(Row, Color, R/C, [ R/C | Moves ]-T) :-
+get_right_moves(_  , _          , _, _/9, T-T) :- !.
+get_right_moves(Row, p(Color, _), _, _/C, T-T) :- arg(C, Row, p(Color, _)), !.
+get_right_moves(Row, Piece      , O, R/C, [ m(Piece, O, R/C) | T ]-T) :- arg(C, Row, p(_, _)), !.
+get_right_moves(Row, Piece      , O, R/C, [ m(Piece, O, R/C) | Moves ]-T) :-
     CPlus is C + 1,
-    get_right_moves(Row, Color, R/CPlus, Moves-T), !.
+    get_right_moves(Row, Piece, O, R/CPlus, Moves-T), !.
--- tests/moves/_horizontal.pl
+++ tests/moves/_horizontal.pl
@@ -2,25 +2,30 @@
 :- use_module('../../src/moves/_horizontal').
 :- use_module('../test_utils').
 
-test(get_horizontal_moves, [setup(start_board(SB)), true(X = [3/2,3/1,3/4,3/5,3/6,3/7,3/8])]) :-
-    get_horizontal_moves(SB, w, 3/3, X-[]).
+test(get_horizontal_moves, [setup(start_board(SB)), true(X = [m(p(w,_),Old,3/2),m(p(w,_),Old,3/1),m(p(w,_),Old,3/4),m(p(w,_),Old,3/5),m(p(w,_),Old,3/6),m(p(w,_),Old,3/7),m(p(w,_),Old,3/8)])]) :-
+    Old = 3/3,
+    get_horizontal_moves(SB, p(w, _), Old, X-[]).
 
-test(get_horizontal_moves, [setup(start_board(SB)), true(X = [4/6,4/5,4/4,4/3,4/2,4/1,4/8])]) :-
-    get_horizontal_moves(SB, w, 4/7, X-[]).
+test(get_horizontal_moves, [setup(start_board(SB)), true(X = [m(p(w,_),Old,4/6),m(p(w,_),Old,4/5),m(p(w,_),Old,4/4),m(p(w,_),Old,4/3),m(p(w,_),Old,4/2),m(p(w,_),Old,4/1),m(p(w,_),Old,4/8)])]) :-
+    Old = 4/7,
+    get_horizontal_moves(SB, p(w, _), Old, X-[]).
 
-test(get_horizontal_moves, [setup(start_board(SB)), true(X = [7/2,7/4])]) :-
-    get_horizontal_moves(SB, w, 7/3, X-[]).
+test(get_horizontal_moves, [setup(start_board(SB)), true(X = [m(p(w,_),Old,7/2),m(p(w,_),Old,7/4)])]) :-
+    Old = 7/3,
+    get_horizontal_moves(SB, p(w, _), Old, X-[]).
 
 test(get_horizontal_moves_corners, [setup(start_board(SB)), true(X = [])]) :-
-    get_horizontal_moves(SB, w, 1/1, X-[]).
+    get_horizontal_moves(SB, p(w, _), 1/1, X-[]).
 
 test(get_horizontal_moves_corners, [setup(start_board(SB)), true(X = [])]) :-
-    get_horizontal_moves(SB, w, 1/8, X-[]).
+    get_horizontal_moves(SB, p(w, _), 1/8, X-[]).
 
-test(get_horizontal_moves_corners, [setup(start_board(SB)), true(X = [8/7])]) :-
-    get_horizontal_moves(SB, w, 8/8, X-[]).
+test(get_horizontal_moves_corners, [setup(start_board(SB)), true(X = [m(p(w,_),Old,8/7)])]) :-
+    Old = 8/8,
+    get_horizontal_moves(SB, p(w, _), Old, X-[]).
 
-test(get_horizontal_moves_corners, [setup(start_board(SB)), true(X = [8/2])]) :-
-    get_horizontal_moves(SB, w, 8/1, X-[]).
+test(get_horizontal_moves_corners, [setup(start_board(SB)), true(X = [m(p(w,_),Old,8/2)])]) :-
+    Old = 8/1,
+    get_horizontal_moves(SB, p(w, _), Old, X-[]).
 
 :- end_tests('moves/_horizontal').
