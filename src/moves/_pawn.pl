c3c0f3ea6905eb5fa42e44e53e20ee8fddeb3489
FKD13 <francisklinck@gmail.com>
2021-06-13 12:10:56 +0200

    change elements in move list - pawn

--- src/moves/_pawn.pl
+++ src/moves/_pawn.pl
@@ -1,45 +1,45 @@
-:- module('_pawn', [get_pawn_moves/4]).
+:- module('_pawn', [get_pawn_moves/5]).
 
 :- use_module('../utils/board_utils').
 
-get_pawn_moves(Board, m(b, Passant), 7/C, Plain-T) :-
-    get_plain_pawn_moves(Board, m(b, Passant), 6/C, Plain-Leap, Valid),
-    add_leap(Board, 5/C, Leap-T, Valid), !.
+get_pawn_moves(Board, p(b, pawn), 7/C, Plain-T, Passant) :-
+    get_plain_pawn_moves(Board, p(b, pawn), 7/C, 6/C, Plain-Leap, Passant, Valid),
+    add_leap(Board, p(b, pawn), 7/C, 5/C, Leap-T, Valid), !.
 
-get_pawn_moves(Board, m(w, Passant), 2/C, Plain-T) :-
-    get_plain_pawn_moves(Board, m(w, Passant), 3/C, Plain-Leap, Valid),
-    add_leap(Board, 4/C, Leap-T, Valid), !.
+get_pawn_moves(Board, p(w, pawn), 2/C, Plain-T, Passant) :-
+    get_plain_pawn_moves(Board, p(w, pawn), 2/C, 3/C, Plain-Leap, Passant, Valid),
+    add_leap(Board, p(w, pawn), 2/C, 4/C, Leap-T, Valid), !.
 
-get_pawn_moves(Board, m(w, Passant), R/C, Moves) :-
+get_pawn_moves(Board, p(w, pawn), R/C, Moves, Passant) :-
     RPlus is R + 1,
-    get_plain_pawn_moves(Board, m(w, Passant), RPlus/C, Moves, _), !.
+    get_plain_pawn_moves(Board, p(w, pawn), R/C, RPlus/C, Moves, Passant, _), !.
 
-get_pawn_moves(Board, m(b, Passant), R/C, Moves) :-
+get_pawn_moves(Board, p(b, pawn), R/C, Moves, Passant) :-
     RMin is R - 1,
-    get_plain_pawn_moves(Board, m(b, Passant), RMin/C, Moves, _), !.
+    get_plain_pawn_moves(Board, p(b, pawn), R/C, RMin/C, Moves, Passant, _), !.
 
 
-get_plain_pawn_moves(Board, MetaData, R/C, Forward-T, Valid) :-
+get_plain_pawn_moves(Board, Piece, OldCoord, R/C, Forward-T, Passant, Valid) :-
     CPlus is C + 1,
     CMin is C - 1,
-    valid_position(Board, R/C, Forward-Strike1, Valid),
-    can_strike(Board, MetaData, R/CPlus, Strike1-Strike2),
-    can_strike(Board, MetaData, R/CMin, Strike2-T).
+    valid_position(Board, Piece, OldCoord, R/C, Forward-Strike1, Valid),
+    can_strike(Board, Piece, OldCoord, Passant, R/CPlus, Strike1-Strike2),
+    can_strike(Board, Piece, OldCoord, Passant, R/CMin, Strike2-T).
 
 
-valid_position(_    , 9/_  , T-T           , no ) :- !.
-valid_position(_    , 0/_  , T-T           , no ) :- !.
-valid_position(Board, Coord, T-T           , no ) :- get_piece_at(Board, Coord, p(_, _)), !.
-valid_position(_    , Coord, [ Coord | T]-T, yes).
+valid_position(_    , _    , _       , 9/_  , T-T, no ) :- !.
+valid_position(_    , _    , _       , 0/_  , T-T, no ) :- !.
+valid_position(Board, _    , _       , Coord, T-T, no ) :- get_piece_at(Board, Coord, p(_, _)), !.
+valid_position(_    , Piece, OldCoord, Coord, [ m(Piece, OldCoord, Coord) | T]-T, yes).
 
-add_leap(Board, Coord, Moves, yes) :- valid_position(Board, Coord, Moves, _).
-add_leap(_    , _    , T-T  , no ).
+add_leap(Board, Piece, OldCoord, Coord, Moves, yes) :- valid_position(Board, Piece, OldCoord, Coord, Moves, _).
+add_leap(_    , _    , _       , _    , T-T  , no ).
 
-can_strike(_    , _          , 9/_  , T-T           ) :- !.
-can_strike(_    , _          , 0/_  , T-T           ) :- !.
-can_strike(_    , _          , _/0  , T-T           ) :- !.
-can_strike(_    , _          , _/9  , T-T           ) :- !.
-can_strike(_    , m(_, Coord), Coord, [ Coord | T]-T) :- !.
-can_strike(Board, _          , Coord, T-T           ) :- get_piece_at(Board, Coord, empty      ), !.
-can_strike(Board, m(Color, _), Coord, T-T           ) :- get_piece_at(Board, Coord, p(Color, _)), !.
-can_strike(Board, _          , Coord, [ Coord | T]-T) :- get_piece_at(Board, Coord, p(_    , _)), !.
+can_strike(_    , _          , _, _   , 9/_  , T-T) :- !.
+can_strike(_    , _          , _, _   , 0/_  , T-T) :- !.
+can_strike(_    , _          , _, _   , _/0  , T-T) :- !.
+can_strike(_    , _          , _, _   , _/9  , T-T) :- !.
+can_strike(_    , Piece      , O, Pass, Pass , [ m(Piece, O, Pass) | T]-T ) :- !.
+can_strike(Board, _          , _, _   , Coord, T-T                                 ) :- get_piece_at(Board, Coord, empty      ), !.
+can_strike(Board, p(Color, _), _, _   , Coord, T-T                                 ) :- get_piece_at(Board, Coord, p(Color, _)), !.
+can_strike(Board, Piece      , O, _   , Coord, [ m(Piece, O, Coord) | T]-T) :- get_piece_at(Board, Coord, p(_    , _)), !.
--- tests/moves/_pawn.pl
+++ tests/moves/_pawn.pl
@@ -3,69 +3,73 @@
 :- use_module('../../src/utils/board_utils').
 :- use_module('../test_utils').
 
-test(valid_position, [forall(empty_generator(Coord)), setup(start_board(SB)), true(X = [Coord])]) :-
-    '_pawn':valid_position(SB, Coord, X-[], yes).
+test(valid_position, [forall(empty_generator(Coord)), setup(start_board(SB)), true(X = [m(Piece, Old, Coord)])]) :-
+    Piece = p(_, pawn),
+    '_pawn':valid_position(SB, Piece, Old, Coord, X-[], yes).
 
 invalid_position_generator(Coord) :- white_generator(Coord).
 invalid_position_generator(Coord) :- black_generator(Coord).
 invalid_position_generator(R/C  ) :- member(R, [0,9]), column(C).
 
 test(invalid_position, [forall(invalid_position_generator(Coord)), setup(start_board(SB))]) :-
-    '_pawn':valid_position(SB, Coord, []-[], no).
+    '_pawn':valid_position(SB, _, _, Coord, []-[], no).
 
 can_strike_invalid_position_generator(R/C) :- member(R, [0,9]), column(C).
 can_strike_invalid_position_generator(R/C) :- member(C, [0,9]), column(R).
 
 test(can_strike_invalid_position, [forall(can_strike_invalid_position_generator(Coord))]) :-
-    '_pawn':can_strike(_, _, Coord, []-[]).
+    '_pawn':can_strike(_, _, _, _, Coord, []-[]).
 
-test(can_strike_en_passant, [forall(board_generator(Coord)), true(X = [Coord])]) :-
-    '_pawn':can_strike(_, m(_, Coord), Coord, X-[]).
+test(can_strike_en_passant, [forall(board_generator(Coord)), true(X = [m(Piece, Old, Coord)])]) :-
+    Piece = p(w, pawn),
+    '_pawn':can_strike(_, Piece, Old, Coord, Coord, X-[]).
 
 test(cant_strike_empty, [forall(empty_generator(Coord)), setup(start_board(SB))]) :-
-    '_pawn':can_strike(SB, m(w, 0/0), Coord, []-[]).
+    '_pawn':can_strike(SB, p(w, pawn), _, 0/0, Coord, []-[]).
 
 test(cant_strike_own_color_white, [forall(white_generator(Coord)), setup(start_board(SB))]) :-
-    '_pawn':can_strike(SB, m(w, 0/0), Coord, []-[]).
+    '_pawn':can_strike(SB, p(w, pawn), _, 0/0, Coord, []-[]).
 
 test(cant_strike_own_color_black, [forall(black_generator(Coord)), setup(start_board(SB))]) :-
-    '_pawn':can_strike(SB, m(b, 0/0), Coord, []-[]).
+    '_pawn':can_strike(SB, p(b, pawn), _, 0/0, Coord, []-[]).
 
-test(can_strike_other_color_white, [forall(black_generator(Coord)), setup(start_board(SB)), true(X = [Coord])]) :-
-    '_pawn':can_strike(SB, m(w, 0/0), Coord, X-[]).
+test(can_strike_other_color_white, [forall(black_generator(Coord)), setup(start_board(SB)), true(X = [m(Piece, Old, Coord)])]) :-
+    Piece = p(w, pawn),
+    '_pawn':can_strike(SB, Piece, Old, 0/0, Coord, X-[]).
 
-test(can_strike_other_color_black, [forall(white_generator(Coord)), setup(start_board(SB)), true(X = [Coord])]) :-
-    '_pawn':can_strike(SB, m(b, 0/0), Coord, X-[]).
+test(can_strike_other_color_black, [forall(white_generator(Coord)), setup(start_board(SB)), true(X = [m(Piece, Old, Coord)])]) :-
+    Piece = p(b, pawn),
+    '_pawn':can_strike(SB, Piece, Old, 0/0, Coord, X-[]).
 
-test(get_plain_pawn_moves, [forall(empty_generator(Coord)), setup(start_board(SB)), true(X = [Coord])]) :-
-    '_pawn':get_plain_pawn_moves(SB, m(_, 0/0), Coord, X-[], yes).
+test(get_plain_pawn_moves, [forall(empty_generator(Coord)), setup(start_board(SB)), true(X = [m(p(_, pawn), Old, Coord)])]) :-
+    '_pawn':get_plain_pawn_moves(SB, p(_, pawn), Old, Coord, X-[], 0/0, yes).
 
-test(get_plain_pawn_moves, [setup(start_board(SB)), true(X = [3/2,3/1])]) :-
+test(get_plain_pawn_moves, [setup(start_board(SB)), true(X = [m(p(w, pawn), Old, 3/2),m(p(w, pawn), Old, 3/1)])]) :-
     set_piece_at(SB, 3/1, p(b, _), Out1),
     set_piece_at(Out1, 3/3, p(w, _), Out2),
-    '_pawn':get_plain_pawn_moves(Out2, m(w, 0/0), 3/2, X-[], yes).
+    '_pawn':get_plain_pawn_moves(Out2, p(w, pawn), Old, 3/2, X-[], 0/0, yes).
 
-test(get_plain_pawn_moves, [setup(start_board(SB)), true(X = [3/2,3/3,3/1])]) :-
+test(get_plain_pawn_moves, [setup(start_board(SB)), true(X = [m(p(w, pawn), Old, 3/2),m(p(w, pawn), Old, 3/3),m(p(w, pawn), Old, 3/1)])]) :-
     set_piece_at(SB, 3/1, p(b, _), Out1),
     set_piece_at(Out1, 3/3, p(b, _), Out2),
-    '_pawn':get_plain_pawn_moves(Out2, m(w, 0/0), 3/2, X-[], yes).
+    '_pawn':get_plain_pawn_moves(Out2, p(w, pawn), Old, 3/2, X-[], 0/0, yes).
 
-test(get_pawn_moves_w_start_positions, [forall(column(C)), setup(start_board(SB)), true(X = [3/C, 4/C])]) :-
-    get_pawn_moves(SB, m(w, 0/0), 2/C, X-[]).
+test(get_pawn_moves_w_start_positions, [forall(column(C)), setup(start_board(SB)), true(X = [m(p(w, pawn), 2/C, 3/C), m(p(w, pawn), 2/C, 4/C)])]) :-
+    get_pawn_moves(SB, p(w, pawn), 2/C, X-[], 0/0).
 
-test(get_pawn_moves_w_middle_positions, [forall((member(R, [3,4,5]),column(C))), setup((start_board(SB), RPlus is R + 1)), true(X = [RPlus/C])]) :-
-    get_pawn_moves(SB, m(w, 0/0), R/C, X-[]).
+test(get_pawn_moves_w_middle_positions, [forall((member(R, [3,4,5]),column(C))), setup((start_board(SB), RPlus is R + 1)), true(X = [m(p(w, pawn), R/C, RPlus/C)])]) :-
+    get_pawn_moves(SB, p(w, pawn), R/C, X-[], 0/0).
 
-test(get_pawn_moves_w_attack_positions, [forall(member(C, [2,3,4,5,6,7])), setup((start_board(SB), CPlus is C + 1, CMin is C - 1)), true(X = [7/CPlus, 7/CMin])]) :-
-    get_pawn_moves(SB, m(w, 0/0), 6/C, X-[]).
+test(get_pawn_moves_w_attack_positions, [forall(member(C, [2,3,4,5,6,7])), setup((start_board(SB), CPlus is C + 1, CMin is C - 1)), true(X = [m(p(w, pawn), 6/C, 7/CPlus), m(p(w, pawn), 6/C, 7/CMin)])]) :-
+    get_pawn_moves(SB, p(w, pawn), 6/C, X-[], 0/0).
 
-test(get_pawn_moves_b_start_positions, [forall(column(C)), setup(start_board(SB)), true(X = [6/C, 5/C])]) :-
-    get_pawn_moves(SB, m(b, 0/0), 7/C, X-[]).
+test(get_pawn_moves_b_start_positions, [forall(column(C)), setup(start_board(SB)), true(X = [m(p(b, pawn), 7/C, 6/C), m(p(b, pawn), 7/C, 5/C)])]) :-
+    get_pawn_moves(SB, p(b, pawn), 7/C, X-[], 0/0).
 
-test(get_pawn_moves_b_middle_positions, [forall((member(R, [4,5,6]),column(C))), setup((start_board(SB), RMin is R - 1)), true(X = [RMin/C])]) :-
-    get_pawn_moves(SB, m(b, 0/0), R/C, X-[]).
+test(get_pawn_moves_b_middle_positions, [forall((member(R, [4,5,6]),column(C))), setup((start_board(SB), RMin is R - 1)), true(X = [m(p(b, pawn), R/C, RMin/C)])]) :-
+    get_pawn_moves(SB, p(b, pawn), R/C, X-[], 0/0).
 
-test(get_pawn_moves_b_attack_positions, [forall(member(C, [2,3,4,5,6,7])), setup((start_board(SB), CPlus is C + 1, CMin is C - 1)), true(X = [2/CPlus, 2/CMin])]) :-
-    get_pawn_moves(SB, m(b, 0/0), 3/C, X-[]).
+test(get_pawn_moves_b_attack_positions, [forall(member(C, [2,3,4,5,6,7])), setup((start_board(SB), CPlus is C + 1, CMin is C - 1)), true(X = [m(p(b, pawn), 3/C, 2/CPlus), m(p(b, pawn), 3/C, 2/CMin)])]) :-
+    get_pawn_moves(SB, p(b, pawn), 3/C, X-[], 0/0).
 
 :- end_tests('moves/_pawn').
