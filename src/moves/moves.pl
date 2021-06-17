054590463248d478dabb2d59049241223d89df7d
FKD13 <francisklinck@gmail.com>
2021-06-18 01:24:24 +0200

    Pass en passant to pawn move generation

--- src/moves/moves.pl
+++ src/moves/moves.pl
@@ -11,30 +11,31 @@
 :- use_module('../utils/board_utils').
 
 get_moves(Board, MW, MB, Color, NormalMoves-T) :-
+    get_metadata(MW, MB, Color, Meta),
+    Meta = m(_, _, _, Passant),
     findall(Moves,(
         get_piece_at(Board, Coord, p(Color, Type)),
-        get_moves_piece(Board, p(Color, Type), Coord, Moves)
+        get_moves_piece(Board, p(Color, Type), Coord, Passant, Moves)
     ), MoveLists),
     foldl([L1-L2, L2-T, L1-T]>>true, MoveLists, X-X, NormalMoves-RokadeMoves),
-    get_metadata(MW, MB, Color, Meta),
     get_rokade_moves(Board, Meta, Color, RokadeMoves-T).
 
-get_moves_piece(Board, p(Color, rook), Coord, Horizontal-T) :- 
+get_moves_piece(Board, p(Color, rook), Coord, _, Horizontal-T) :- 
     get_horizontal_moves(Board, p(Color, rook), Coord, Horizontal-Vertical),
     get_vertical_moves(Board, p(Color, rook), Coord, Vertical-T), !.
-get_moves_piece(Board, p(Color, knight), Coord, Moves) :-
+get_moves_piece(Board, p(Color, knight), Coord, _, Moves) :-
     get_knight_moves(Board, p(Color, knight), Coord, Moves), !.
-get_moves_piece(Board, p(Color, bishop), Coord, Moves) :- 
+get_moves_piece(Board, p(Color, bishop), Coord, _, Moves) :- 
     get_diagonal_moves(Board, p(Color, bishop), Coord, Moves), !.
-get_moves_piece(Board, p(Color, queen), Coord, Horizontal-T) :- 
+get_moves_piece(Board, p(Color, queen), Coord, _, Horizontal-T) :- 
     get_horizontal_moves(Board, p(Color, queen), Coord, Horizontal-Vertical),
     get_vertical_moves(Board, p(Color, queen), Coord, Vertical-Diagonal),
     get_diagonal_moves(Board, p(Color, queen), Coord, Diagonal-T), !.
-get_moves_piece(Board, p(Color, king), Coord, Moves) :- 
+get_moves_piece(Board, p(Color, king), Coord, _, Moves) :- 
     get_surrounding_moves(Board, p(Color, king), Coord, Moves), !. 
-get_moves_piece(Board, p(Color, pawn), Coord, Moves) :- 
-    get_pawn_moves(Board, p(Color, pawn), Coord, Moves, no), !.
-    % TODO: pass correct en passant.
+get_moves_piece(Board, p(Color, pawn), Coord, Passant, Moves) :- 
+    get_pawn_moves(Board, p(Color, pawn), Coord, Moves, Passant), !.
+
 
 get_metadata(MW, _ , w, MW).
 get_metadata(_ , MB, b, MB).
