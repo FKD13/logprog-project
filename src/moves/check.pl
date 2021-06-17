160246555e495c30f426ee34f9e7a0f33a9ab4e4
FKD13 <francisklinck@gmail.com>
2021-06-18 01:47:41 +0200

    add docs to moves predicates

--- src/moves/check.pl
+++ src/moves/check.pl
@@ -1,7 +1,18 @@
 :- module('state', [check/3]).
 
+/** <module> check - Is check?
+*/
+
 :- use_module('../utils/board_utils').
 
+
+%!  check(+Board, +Color, +Moves)
+%
+%   Given the moves of the enemy player, unify if the current player is check.
+%
+%   @arg Board The board to check available moves on.
+%   @arg Color The color of the current player.
+%   @arg Moves The possible moves. This is a difference list.
 check(Board, Color, Moves) :-
     get_piece_at(Board, Coord, p(Color, king)),
     member(m(_, _, Coord), Moves).
--- src/moves/moves.pl
+++ src/moves/moves.pl
@@ -1,5 +1,8 @@
 :- module('moves', [get_moves/5]).
 
+/** <module> moves - Get all moves.
+*/
+
 :- use_module('_diagonal').
 :- use_module('_horizontal').
 :- use_module('_knight').
@@ -10,6 +13,18 @@
 
 :- use_module('../utils/board_utils').
 
+
+%!  get_moves(+Board, +MetaDataWhite, +MetaDataBlack, +Color, -Moves)
+%
+%   Get all possible moves given a board configuration and its metadata.
+%   The metadata is used to determin if some special moves like the rokade and 
+%   en passant should be taken.
+%
+%   @arg Board The board to check available moves on.
+%   @arg MetaDataWhite Metadata for the white player.
+%   @arg MetaDataBlack Metadata for the black player.
+%   @arg Color The currently playing color.
+%   @arg Moves The possible moves. This is a difference list.
 get_moves(Board, MW, MB, Color, NormalMoves-T) :-
     get_metadata(MW, MB, Color, Meta),
     Meta = m(_, _, _, Passant),
@@ -20,6 +35,16 @@ get_moves(Board, MW, MB, Color, NormalMoves-T) :-
     foldl([L1-L2, L2-T, L1-T]>>true, MoveLists, X-X, NormalMoves-RokadeMoves),
     get_rokade_moves(Board, Meta, Color, RokadeMoves-T).
 
+
+%!  get_moves_piece(+Board, +Piece, +Coord, +Passant, -Moves)
+%
+%   Given a Piece and it's position, determin all possible moves for that piece.
+%
+%   @arg Board The board to check available moves on.
+%   @arg Piece The piece at the coordinate location.
+%   @arg Coord The location of the piece.
+%   @arg Passant The possibility of a en passant.
+%   @arg Moves The possible moves. This is a difference list.
 get_moves_piece(Board, p(Color, rook), Coord, _, Horizontal-T) :- 
     get_horizontal_moves(Board, p(Color, rook), Coord, Horizontal-Vertical),
     get_vertical_moves(Board, p(Color, rook), Coord, Vertical-T), !.
@@ -37,5 +62,13 @@ get_moves_piece(Board, p(Color, pawn), Coord, Passant, Moves) :-
     get_pawn_moves(Board, p(Color, pawn), Coord, Moves, Passant), !.
 
 
+%!  get_metadata(+MetaDataWhite, +MetaDataBlack, +Color, +MetaData)
+%
+%   Given a color, provide the corresponding metadata.
+%
+%   @arg MetaDataWhite Metadata for the white player.
+%   @arg MetaDataBlack Metadata for the black player.
+%   @arg Color The given color.
+%   @arg MetaData The MetaData corresponding with the Color.
 get_metadata(MW, _ , w, MW).
 get_metadata(_ , MB, b, MB).
