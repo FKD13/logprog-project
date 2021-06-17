2f51327ae7720a5e811528afb4c58dbc6ad808d0
Francis <francisklinck@gmail.com>
2021-06-17 18:04:54 +0200

    add docs in main module

--- src/main.pl
+++ src/main.pl
@@ -13,9 +13,15 @@ The main module defines rules used to start the chess program.
 */
 
 
-%! main
+%!  main(+Argv)
+%
+%   Run the program.
+%   Accepts:
+%   - No arguments: run the program normally.
+%   - TEST argument: run the program in test mode.
+%   - Else: repot invalid arguments.
 % 
-% The main rule, this starts the program.
+%   @arg Argv The command line arguments.
 main([]) :- !,
     read_string(user_input, _, Str),
     string_codes(Str, Codes),
@@ -35,9 +41,24 @@ main(['TEST']) :- !,
     get_moves(Board, Color, Moves-[]),
     write_test_results(Board, M1, M2, Moves),
     halt(0).
-main(_) :- !,
-    nl, write("Invalid Aguments"), nl, halt(1).
+main(_) :- !, nl, write("Invalid Aguments"), nl, halt(1).
 
+
+%!  write_test_results(+Board, +MetaDataWhite, +MetaDataBlack, +Moves)
+%
+%   Given a Board and it's metadata, for each move:
+%   - Check id the move is valid.
+%   - Apply to the Board.
+%   - Print the Board to stdout.
+%   This while separating all boards by a 
+%   ~~~
+%   nl, write("~"), nl
+%   ~~~
+%
+%   @arg Board
+%   @arg MetaDataWhite Metadata for the white player.
+%   @arg MetaDataBlack Metadata for the black player.
+%   @arg Moves The moves to apply to the board.
 write_test_results(_    , _ , _ , []).
 write_test_results(Board, M1, M2, [Move]) :-
     move(Board, M1, M2, Move, NB, NM1, NM2),
@@ -49,5 +70,13 @@ write_test_results(Board, M1, M2, [Move | Moves]) :-
     write("~"), nl,
     write_test_results(Board, M1, M2, Moves).
 
+
+%!  color(+MetaDataWhite, +MetaDataBlack, -Color)
+%
+%   Given 2 sets of metadata, get the playing color.
+%
+%   @arg MetaDataWhite Metadata for the white player.
+%   @arg MetaDataBlack Metadata for the black player.
+%   @arg Color The currently playing color.
 color(m(_, _, yes, _), m(_, _, no , _), w).
 color(m(_, _, no , _), m(_, _, yes, _), b).
