07810a8b147ea11b15bfefd10bc678033208904e
FKD13 <francisklinck@gmail.com>
2021-06-17 23:56:04 +0200

    implement rokade

--- src/moves/_rokade.pl
+++ src/moves/_rokade.pl
@@ -4,32 +4,69 @@
 :- use_module('_vertical').
 :- use_module('_diagonal').
 :- use_module('_knight').
+:- use_module('_surroundings').
 
 :- use_module('../utils/board_utils').
 :- use_module('../utils/color').
 
-get_rokade_moves(Board, m(Queen, King, yes, _), Color, Moves) :- 
-    next_color(Color, NextColor),
-    long_rokade(Queen, Board, Color, NextColor, Moves),
-    short_rokade(King, Board, Color, NextColor, Moves).
+get_rokade_moves(Board, m(Queen, King, yes, _), Color, Long-T) :- 
+    long_rokade(Queen, Board, Color, Long-Short),
+    short_rokade(King, Board, Color, Short-T_), T_ = T.
 
-long_rokade(no , _, _, _, T-T).
-long_rokade(yes, _, _, _, T-T).
+long_rokade(no , _    , _, T-T  ).
+long_rokade(yes, Board, w, Moves) :- long_rokade_color(Board, w, b, 1, Moves).
+long_rokade(yes, Board, b, Moves) :- long_rokade_color(Board, b, w, 8, Moves).
 
-short_rokade(no , _, _, _, T-T).
-short_rokade(yes, _, _, _, T-T).
+long_rokade_color(Board, Color, NextColor, R, [ r(m(p(Color, king), R/5, R/3), m(p(Color, rook), R/1, R/4)) | T]-T) :-
+    safe(Board, R/5, Color, NextColor),
+    get_piece_at(Board, R/1, p(Color, rook)),
+    safe_and_empty(Board, R/2, Color, NextColor),
+    safe_and_empty(Board, R/3, Color, NextColor),
+    safe_and_empty(Board, R/4, Color, NextColor),
+    get_piece_at(Board, R/5, p(Color, king)).
+long_rokade_color(_, _, _, _, T-T).
+
+short_rokade(no , _    , _, T-T  ).
+short_rokade(yes, Board, w, Moves) :- short_rokade_color(Board, w, b, 1, Moves).
+short_rokade(yes, Board, b, Moves) :- short_rokade_color(Board, b, w, 8, Moves).
+
+short_rokade_color(Board, Color, NextColor, R, [ r(m(p(Color, king), R/5, R/7), m(p(Color, rook), R/8, R/6)) | T]-T) :-
+    safe(Board, R/5, Color, NextColor),
+    get_piece_at(Board, R/5, p(Color, king)),
+    safe_and_empty(Board, R/6, Color, NextColor),
+    safe_and_empty(Board, R/7, Color, NextColor),
+    get_piece_at(Board, R/8, p(Color, rook)).
+short_rokade_color(_, _, _, _, T-T).
+
+safe_and_empty(Board, Coord, Color, NextColor) :-
+    get_piece_at(Board, Coord, empty),
+    safe(Board, Coord, Color, NextColor).
 
 safe(Board, R/C, Color, NextColor) :- 
-    get_knight_moves(    Board, p(Color, knight), R/C, L1-L2),
-    get_horizontal_moves(Board, p(Color, rook)  , R/C, L2-L3),
-    get_vertical_moves(  Board, p(Color, rook)  , R/C, L3-L4),
-    get_diagonal_moves(  Board, p(Color, bishop), R/C, L4-L5),
+    \+ danger_vertical(Board   , R/C, Color, NextColor),
+    \+ danger_diagonal(Board   , R/C, Color, NextColor),
+    \+ danger_knight(Board     , R/C, Color, NextColor),
+    \+ danger_surrounding(Board, R/C, Color, NextColor),
     CPlus is C + 1, CMin is C - 1,
-    pawns(Board, R/CPlus, Color, L-L),
-    pawns(Board, R/C    , Color, L-L),
-    pawns(Board, R/CMin , Color, L-L).
+    \+ danger_pawn(Board, R/CPlus, Color),
+    \+ danger_pawn(Board, R/CMin , Color).
 
-pawns(_    , Coord, _, T-T) :- outside_board(Coord), !.
-pawns(Board, R/C  , w, [ m(p(b, pawn), _, _) | T]-T) :- RPlus is R + 1, get_piece_at(Board, RMin/C, p(b, pawn)), !.
-pawns(Board, R/C  , b, [ m(p(w, pawn), _, _) | T]-T) :- RMin  is R - 1, get_piece_at(Board, RMin/C, p(w, pawn)), !.
-pawns(Board, R/C  , _, T-T).
+danger_pawn(Board, R/C, w) :- RPlus is R + 1, get_piece_at(Board, RPlus/C, p(b, pawn)).
+danger_pawn(Board, R/C, b) :- RMin  is R - 1, get_piece_at(Board, RMin/C , p(w, pawn)).
+
+danger_surrounding(Board, R/C, Color, NextColor) :-
+    danger_X(get_surrounding_moves, Board, R/C, Color, NextColor, king, [king]).
+
+danger_vertical(Board, Coord, Color, NextColor) :- 
+    danger_X(get_vertical_moves, Board, Coord, Color, NextColor, rook, [queen, rook]).
+
+danger_diagonal(Board, Coord, Color, NextColor) :-
+    danger_X(get_diagonal_moves, Board, Coord, Color, NextColor, bishop, [queen, bishop]).
+
+danger_knight(Board, Coord, Color, NextColor) :-
+    danger_X(get_knight_moves, Board, Coord, Color, NextColor, knight, [knight]).
+
+danger_X(MoveGenerator, Board, Coord, Color, NextColor, Type, DangerTypes) :-
+    call(MoveGenerator, Board, p(Color, Type), Coord, Moves-[]), !,
+    member(m(p(NextColor, OtherType), _, _), Moves),
+    member(OtherType, DangerTypes).
